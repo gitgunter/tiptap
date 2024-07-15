d9572d664f798c0c432b2685713b9c4925bc3a63
gitgunter <keylordani@gmail.com>
2024-07-15 14:13:47 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import TiptapEditor from './components/TiptapEditor';
+
+function App() {
+  return (
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh'
+      }}
+    >
+      <TiptapEditor />
+    </div>
+  );
+}
+
+export default App;
